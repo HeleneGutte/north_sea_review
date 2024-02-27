95b3e7654a5bad5d267d4d9c56edb0d971403ae5
Helene Gutte <helene.gutte@uni-hamburg.de>
2024-02-27 10:53:50 +0100

    Set up paper analysis script.

---
+++ Paper_analysis.R
@@ -0,0 +1,15 @@
+# Analysis for North Sea review ----
+
+# 1. packages and data ----
+library(tidyverse)
+answers_meta_final <- read_csv("Data/answers_meta_final.csv")
+
+# 2. plotting settings ----
+my_colours <- c("invasive species" = "turquoise", "climate change" = "orange", 
+                "direct exploitation" = "blue", "sea use change" = "gold", 
+                "pollution" = "purple")
+
+# 3. prepare data table ----
+
+
+
